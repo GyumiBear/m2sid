a2c735f9f927af7f3590a1d2d7b264bfa1c8fc27
GyumiBear <75048244+GyumiBear@users.noreply.github.com>
2025-03-01 02:03:29 +0100

    Rajout des données pur les tests E2E

---
+++ src/test/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO participant (nom, prenom) VALUES ('Doe', 'John');
+INSERT INTO sondage (nom, description, participant_id) VALUES ('Sondage Test', 'Description', 1);
+INSERT INTO commentaire (commentaire, sondage_id, participant_id) VALUES ('Premier commentaire', 1, 1);
